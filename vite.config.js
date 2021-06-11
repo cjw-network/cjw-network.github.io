2d60a92e3a5a8be5502b1efd0af2c28664c34c51
dfritschy <donat@fritschy.ch>
2021-06-11 17:17:23 +0200

    feat: add posthtml plugin to include html fragments

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 // vite.config.js
 
 const { resolve } = require('path')
+const { posthtmlPlugin } = require('vite-plugin-posthtml');
+// https://github.com/chromeos/static-site-scaffold-modules/tree/main/modules/vite-plugin-posthtml
 
 export default {
     root: "./src",
@@ -11,8 +13,19 @@ export default {
                 de_index: resolve(__dirname, 'src/index.html'),
                 de_contact: resolve(__dirname, 'src/contact.html'),
                 de_data: resolve(__dirname, 'src/impress.html'),
-                en: resolve(__dirname, 'src/en/index.html')
+                en_index: resolve(__dirname, 'src/en/index.html'),
+                en_contact: resolve(__dirname, 'src/en/contact.html'),
+                en_data: resolve(__dirname, 'src/en/impress.html'),
             }
         }
     },
+    plugins: [
+        posthtmlPlugin({
+            plugins: [
+                require('posthtml-include')(),
+                //require('posthtml-favicons')()
+            ],
+            options: {}
+        })
+    ]
 }
